3d98c913b5ee6c85981720d2d2b32f6e8fe52939
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-09-15 19:47:38 +0900

    Add files via upload

---
+++ 1018_30101330(AC).cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//#include <iostream>
+#include <stdio.h>
+///using namespace std;
+
+int main() {
+	int h, n;
+	scanf("%d:%d", &h,&n);
+	printf("%d:%d", h,n);
+	return 0;
+
+}
