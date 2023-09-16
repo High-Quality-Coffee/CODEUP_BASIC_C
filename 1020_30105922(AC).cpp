ec01af4dafbbb42704cbaf647535391740d11a71
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-09-17 00:17:42 +0900

    Add files via upload

---
+++ 1020_30105922(AC).cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//#include <iostream>
+#include <stdio.h>
+///using namespace std;
+
+int main() {
+	int first,second;
+
+	scanf("%d-%d", &first,&second);
+	if (first / 100000 >= 1) {
+		printf("%d%d", first, second);
+	}
+
+	else if (first % 100000 < 100000 && first % 100000 >= 10000) {
+		printf("0");
+		printf("%d%d", first, second);
+	}
+
+	else if (first % 100000 < 10000 && first % 100000 >= 1000) {
+		printf("00");
+		printf("%d%d", first, second);
+	}
+
+	else if (first % 100000 < 1000 && first % 100000 >= 100) {
+		printf("000");
+		printf("%d%d", first, second);
+	}
+
+
+	return 0;
+
+}
