1742480fcafa042d1981f6a7cdce220b7b0234f3
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-09-22 00:45:48 +0900

    Add files via upload

---
+++ 1022_30106385(AC).cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+//#include <stdio.h>
+using namespace std;
+
+int main() {
+	
+	string s;
+	getline(cin, s);
+	cout << s;
+
+	return 0;
+
+}
