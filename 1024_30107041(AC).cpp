a43a7af5f4ae00d8fbcfb5fd9994adce2ee49ab8
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-09-27 19:39:52 +0900

    Add files via upload

---
+++ 1024_30107041(AC).cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+//#include <stdio.h>
+using namespace std;
+
+int main() {
+	string s;
+	int length;
+	getline(cin, s);
+	length = s.length();
+
+	for (int i = 0; i < length; i++) {
+		cout << "'" << s[i] <<"'" << endl;
+	}
+
+	return 0;
+
+}
