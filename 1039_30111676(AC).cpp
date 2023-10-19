75da822a3d954956a49332a3c3011b9fb07294f3
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-10-19 23:11:50 +0900

    Add files via upload

---
+++ 1039_30111676(AC).cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	long long int n,m,s;
+	scanf("%lld %lld", &n, &m);
+	s = n + m;
+
+	printf("%lld", s);
+
+	return 0;
+
+}
