453376bee7b908580dc975ee8fa33e70633df2da
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-10-20 23:14:42 +0900

    Add files via upload

---
+++ 1040_30111688(AC).cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	cout << n * -1;
+
+	return 0;
+
+}
