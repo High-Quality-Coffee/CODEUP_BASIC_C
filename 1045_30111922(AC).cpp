67a63e65b622b635275ea82426cf4f57b7024133
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-10-27 23:17:44 +0900

    Add files via upload

---
+++ 1045_30111922(AC).cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	long long int a,b;
+	cin >> a>>b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	cout.precision(2);
+	cout << fixed;
+	cout << a / (double)b;
+
+
+
+	return 0;
+
+}
