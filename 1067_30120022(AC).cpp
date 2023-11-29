8ca64d285fa36468a106cb3bc2d6fdfb54aa1002
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-11-30 02:02:59 +0900

    Add files via upload

---
+++ 1067_30120022(AC).cpp
@@ -0,0 +1,35 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int a;
+	cin >> a;
+	
+	if (a >0) { 
+		cout << "plus" << endl; 
+	if (a % 2 == 0)cout << "even";
+
+	else cout << "odd";
+
+	}
+	
+	if (a < 0) {
+		cout << "minus" << endl;
+		if (a % 2 == 0)cout << "even";
+
+		else cout << "odd";
+
+	}
+	
+
+	
+	
+	return 0;
+
+}
