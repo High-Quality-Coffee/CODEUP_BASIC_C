62a9b73234de0b9fea68019627eb28f51757cde7
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-02 18:21:31 +0900

    Add files via upload

---
+++ 1068_30120044(AC).cpp
@@ -0,0 +1,20 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int a;
+	cin >> a;
+		
+	if (a >= 90 && a <= 100)cout << "A";
+	else if (a >= 70 && a <= 89)cout << "B";
+	else if (a >= 40 && a <= 69)cout << "C";
+	else if (a >= 0 && a <= 39)cout << "D";
+	return 0;
+
+}
