5a604f7dfe976f0c3f17f95500a9a4fdb5372a39
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-03 23:25:39 +0900

    Add files via upload

---
+++ 1069_30120068(AC).cpp
@@ -0,0 +1,28 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	char a;
+	cin >> a;
+
+	switch (a) {
+
+	case 'A': cout << "best!!!"; break;
+	case 'B':cout << "good!!"; break;
+	case 'C':cout << "run!"; break;
+	case 'D':cout << "slowly~"; break;
+	default: cout << "what?"; break;
+
+	}
+
+
+
+	return 0;
+
+}
