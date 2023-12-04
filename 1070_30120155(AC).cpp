d72a77c57aa08ea7e69482c704640a6cf5c096cb
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-05 02:17:52 +0900

    Add files via upload

---
+++ 1070_30120155(AC).cpp
@@ -0,0 +1,30 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int a;
+	cin >> a;
+
+	switch (a) {
+
+	case 12: 
+	case 1:
+	case 2:cout << "winter"; break;
+	case 3:
+	case 4:
+	case 5:cout << "spring"; break;
+	case 6:
+	case 7:
+	case 8:cout << "summer"; break;
+	default: cout << "fall"; break;
+
+	}
+	return 0;
+
+}
