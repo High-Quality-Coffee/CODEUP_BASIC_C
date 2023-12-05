c708838b645f3d2a2aead779dae42208845b8495
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-06 03:32:22 +0900

    Add files via upload

---
+++ 1071_30120260(AC).cpp
@@ -0,0 +1,21 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int a;
+
+	while (1) {
+		cin >> a;
+		if (a == 0) break;
+		cout << a << endl;
+	}
+
+	return 0;
+
+}
