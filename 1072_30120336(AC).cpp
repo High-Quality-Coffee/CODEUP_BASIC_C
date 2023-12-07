ab5838402a2c12682f797330e01036b8c348526e
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-08 04:18:36 +0900

    Add files via upload

---
+++ 1072_30120336(AC).cpp
@@ -0,0 +1,22 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+
+	int n,m;
+
+	cin >> n;
+reget:
+	cin >> m;
+	cout << m << endl;
+	n--;
+	if (n!= 0)goto reget;
+
+	return 0;
+
+}
