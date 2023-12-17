6d7ad07aaacf49c3239dbdec04fea40140e91558
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2023-12-17 23:33:13 +0900

    Add files via upload

---
+++ 1076_30129330(AC).cpp
@@ -0,0 +1,22 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {
+	char first = 'a';
+	char  c;
+	cin >> c;
+
+
+	while (first<c+1) {
+		cout<<first<<" ";
+		first++;
+	}
+
+	return 0;
+
+}
