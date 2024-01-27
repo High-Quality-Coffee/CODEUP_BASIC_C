00f8d89c3e46678c328e93b4cf66d1795d2629fe
박규원 <gw2000sk@korea.ac.kr>
2024-01-28 00:08:13 +0900

    Add files via upload

---
+++ 1077_30129366(AC).cpp
@@ -0,0 +1,22 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {	
+
+	int set = 0;
+	int n;
+	cin >> n;
+
+	while (set<=n) {
+		cout << set << endl;
+		set++;
+	}
+
+	return 0;
+
+}
