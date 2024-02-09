79ad8f231638a8633b8437907779add14849e977
박규원 <gw2000sk@korea.ac.kr>
2024-02-10 01:50:40 +0900

    Add files via upload

---
+++ 1080_30142020(AC).cpp
@@ -0,0 +1,26 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+//#include <string>
+//#include <stdio.h>
+//#include <math.h>
+using namespace std;
+
+int main() {	
+
+	int n;
+	int sum = 0, i = 1;
+	cin >> n;
+
+
+	while (1) {
+		sum += i;
+		if (sum >= n)break;
+		i++;
+	}
+
+	cout << i;
+
+	return 0;
+
+}
