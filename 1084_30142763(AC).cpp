2d0051dd70bafc3fb3946aadd0dec81fd58609f9
박규원 <gw2000sk@korea.ac.kr>
2024-02-18 22:08:42 +0900

    Add files via upload

---
+++ 1084_30142763(AC).cpp
@@ -0,0 +1,24 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main() {	
+
+	int r, g, b;
+	int count = 0;
+	cin >> r >> g >> b;
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < g; j++) {
+			for (int k = 0; k < b; k++) {
+				printf("%d %d %d\n", i, j, k);
+				count++;
+			}
+		}
+	}
+	cout << count;
+
+	return 0;
+
+}
