57c9b2cde9c3457d15db21f15c73cad1fc8cd271
박규원 <gw2000sk@korea.ac.kr>
2024-02-19 16:40:31 +0900

    Add files via upload

---
+++ 1085_30143643(AC).cpp
@@ -0,0 +1,24 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+
+int main() {	
+
+	double h, b, c, s;
+	double byte, kb, mb;
+
+	cin >> h >> b >> c >> s;
+
+
+	byte = (h * b*c*s) /  8;
+	kb = byte / 1024;
+	mb = kb /1024;
+
+	cout.precision(1);
+	cout << fixed;
+	cout << mb<<" MB";
+
+	return 0;
+
+}
