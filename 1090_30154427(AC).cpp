199a2bd944d013a35c92fd2f56bfb7f7bbb23c02
박규원 <gw2000sk@korea.ac.kr>
2024-02-28 23:58:11 +0900

    Add files via upload

---
+++ 1090_30154427(AC).cpp
@@ -0,0 +1,17 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	long long int a, r, n,t;
+
+	cin >> a >> r >> n;
+	t= a * pow(r, n - 1);
+	cout << t;
+
+	return 0;
+
+}
