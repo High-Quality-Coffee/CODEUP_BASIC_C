206080e694ff2b4ea1784486906c0aa52340139f
박규원 <gw2000sk@korea.ac.kr>
2024-03-01 21:39:19 +0900

    Add files via upload

---
+++ 1091_30154596(AC).cpp
@@ -0,0 +1,21 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	long long int a, m, d, n;
+
+	cin >> a >> m >> d >> n;
+
+	for (int i = 2; i <= n; i++) {
+		a = a * m + d;
+	}
+
+	cout << a;
+
+	return 0;
+
+}
