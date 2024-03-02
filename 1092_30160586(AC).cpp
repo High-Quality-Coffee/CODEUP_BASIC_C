dedcdb602d36943a03f25b3141e9320bc8b13746
박규원 <gw2000sk@korea.ac.kr>
2024-03-02 23:30:03 +0900

    Add files via upload

---
+++ 1092_30160586(AC).cpp
@@ -0,0 +1,19 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+	int a, b, c, day;
+	cin >> a >> b >> c;
+	
+	day = 1;
+	while (day%a != 0 || day % b != 0 || day % c != 0)day++;
+
+	cout << day;
+
+	return 0;
+
+}
